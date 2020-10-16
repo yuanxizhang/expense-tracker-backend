3857e1fef3b8197c8e8589ffaf95c6996e9ba557
yuanxizhang <yuanxi.zhang@gmail.com>
2020-10-16 15:33:58 -0700

    Add fields to accounts table and transactions table

--- db/migrate/20201013012440_create_accounts.rb
+++ db/migrate/20201013012440_create_accounts.rb
@@ -1,7 +1,8 @@
 class CreateAccounts < ActiveRecord::Migration[6.0]
   def change
     create_table :accounts do |t|
-
+      t.string :name, null: false
+      t.float :balance, null: false
       t.timestamps
     end
   end
--- db/migrate/20201013012658_create_transactions.rb
+++ db/migrate/20201013012658_create_transactions.rb
@@ -1,6 +1,11 @@
 class CreateTransactions < ActiveRecord::Migration[6.0]
   def change
     create_table :transactions do |t|
+      t.float :amount, null: false
+      t.string :kind, null: false
+      t.datetime :date, null: false
+      t.string :description
+      t.references :account, foreign_key: true
 
       t.timestamps
     end
