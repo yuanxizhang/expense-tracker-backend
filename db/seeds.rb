f077d8e20ce38ded0d53b1e1565b9c3d06135c7b
yuanxizhang <yuanxi.zhang@gmail.com>
2020-10-29 11:04:52 -0700

    update seed.rb, create migration table

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
+
 # Examples:
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Account.destroy_all
+Transaction.destroy_all
+
+phil = Account.create({name: "Phil", balance: $12345.60})
+alfred = Transaction.create({name: "Alfred", balance: $54321.00})
+phil.transactions.create({amount: 2000.00, kind: "withdraw", date: Time.new.strftime("%Y-%m-%d %H:%M:%S"), description: "rent"})
+phil.transactions.create({amount: 200.00, kind: "withdraw", date: Time.new.strftime("%Y-%m-%d %H:%M:%S"), description: "phone bill"})
+phil.transactions.create({amount: 6000.00, kind: "deposit", date: Time.new.strftime("%Y-%m-%d %H:%M:%S"), description: "pay check"})
+alfred.transactions.create({amount: 3000.00, kind: "withdraw", date: Time.new.strftime("%Y-%m-%d %H:%M:%S"), description: "rent"})
+alfred.transactions.create({amount: 300.00, kind: "withdraw", date: Time.new.strftime("%Y-%m-%d %H:%M:%S"), description: "phone bill"})
+alfred.transactions.create({amount: 9000.00, kind: "deposit", date: Time.new.strftime("%Y-%m-%d %H:%M:%S"), description: "pay check"})
